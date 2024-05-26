605e7ac395ff7a35c8304a93e0531f2ae3f8c9d3
DESKTOP-2MTF3A5\lbotelho <l_botelho@hotmail.com>
2024-05-26 17:07:23 +0100

    crop board imagem original

---
+++ SS_OpenCV/Models/Coord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG_OpenCV.Models
+{
+    internal class Coord
+    {
+        public Coord(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public int X { get; set; }
+        public int Y { get; set; }  
+    }
+}
--- SS_OpenCV/Services/BoardCroper.cs
+++ SS_OpenCV/Services/BoardCroper.cs
@@ -9,27 +9,30 @@ using System.Drawing;
 
 using Emgu.CV.Structure;
 using Emgu.CV;
+using CG_OpenCV.Models;
 
 namespace CG_OpenCV.Services
 {
     internal class BoardCroper
     {
-        Image<Bgr, Byte> fullImg = null; // working image
+        Image<Bgr, Byte> imgCopy = null; // working image
+        Image<Bgr, Byte> imgOriginal = null; // working image
         public BoardCroper(Image<Bgr, Byte> img)
         {
-            this.fullImg = img.Copy();
+            this.imgOriginal = img;
+            this.imgCopy = img.Copy();
         }
 
         public Image<Bgr, Byte> CropBoard()
         {
-            var imgCopy = fullImg.Copy();
+            var imgCopy = this.imgCopy.Copy();
 
-            var coords = this.FindBoardCoord();
+            var coords = this.FindBoardCoords();
 
-            int width = coords[1].Value.Item1 - coords[0].Value.Item1;
-            int height = coords[1].Value.Item2 - coords[0].Value.Item2;
+            int width = Math.Abs(coords[0].X - coords[1].X);
+            int height = Math.Abs(coords[0].Y - coords[1].Y);
 
-            var croppedImg = fullImg.GetSubRect(new Rectangle(coords[0].Value.Item1, coords[0].Value.Item2, width, height));
+            var croppedImg = this.imgOriginal.GetSubRect(new Rectangle(coords[0].X, coords[1].Y, width, height));
 
             string relativePath = Path.Combine("..", "..", "ImagensTeste/fullImageAfterCrop.png");
             string absolutePath = Path.GetFullPath(relativePath);
@@ -38,58 +41,7 @@ namespace CG_OpenCV.Services
             return imgCopy;
         }
 
-        private (int, int)?[] FindBoardCoord()
-        {
-            ImageClass.ConvertToBW_Otsu(fullImg);
-
-            //save image locally
-            string relativePath = Path.Combine("..", "..", "ImagensTeste/imgbin1.png");
-            string absolutePath = Path.GetFullPath(relativePath);
-
-            fullImg.Bitmap.Save(absolutePath, ImageFormat.Png);
-
-            var coordinates = new (int, int)?[2];
-
-
-
-            coordinates[0] = FindFirstWhitePixel(fullImg.Bitmap);
-            coordinates[1] = FindLastWhitePixel(fullImg.Bitmap);
-
-            return coordinates;
-        }
-
-        private (int x, int y)? FindFirstWhitePixel(Bitmap bitmap)
-        {
-            for (int y = 0; y < bitmap.Height; y++)
-            {
-                for (int x = 0; x < bitmap.Width; x++)
-                {
-                    Color pixelColor = bitmap.GetPixel(x, y);
-                    if (pixelColor.R == 255 && pixelColor.G == 255 && pixelColor.B == 255)
-                    {
-                        return (x, y);
-                    }
-                }
-            }
-            return null;
-        }
-
-        private (int x, int y)? FindLastWhitePixel(Bitmap bitmap)
-        {
-            for (int y = bitmap.Height - 1; y >= 0; y--)
-            {
-                for (int x = bitmap.Width - 1; x >= 0; x--)
-                {
-                    Color pixelColor = bitmap.GetPixel(x, y);
-                    if (pixelColor.R == 255 && pixelColor.G == 255 && pixelColor.B == 255)
-                    {
-                        return (x, y);
-                    }
-                }
-            }
-            return null;
-        }
-        public static void ColorToHSV(Color color, out double hue, out double saturation, out double value)
+        public void ColorToHSV(Color color, out double hue, out double saturation, out double value)
         {
             int max = Math.Max(color.R, Math.Max(color.G, color.B));
             int min = Math.Min(color.R, Math.Min(color.G, color.B));
@@ -97,32 +49,17 @@ namespace CG_OpenCV.Services
             saturation = (max == 0) ? 0 : 1d - (1d * min / max);
             value = max / 255d;
         }
-        public static (int x, int y)?[] findBoard(Image<Bgr, byte> img)
+        public Coord[] FindBoardCoords()
         {
+            int minX = -1, maxX = -1, minY = -1, maxY = -1;
+
             unsafe
             {
-                // direct access to the image memory(sequencial)
-                // direcion top left -> bottom right
-                // aceder diretamente à memória
-
-
-                MIplImage m = img.MIplImage;
+                MIplImage m = imgCopy.MIplImage;
                 byte* dataPtr = (byte*)m.imageData.ToPointer(); // Pointer to the image
                 byte blue, green, red;
-                /*
-                 * MIplImage contém vários params que descrevem como está organizada a imagem
-                width largura da imagem
-                widthStep indica qual a largura de total de uma linha (bytes)
-                nChannels indica o número de canais de cor (RGB = 3)
-                trabalhar com a estrutura dataptr for necessário avançar ou recuar uma linha
-                usar o widthStep
-                avançar uma linha de pixeis completa dataPtr += m.widthStep
-                avançar numero de pixeis de alinhamento para passar a linha seguinte
-                int padding = m.widthStep - m.nChannels * m.width;
-                dataPtr += padding;
-                */
-                int width = img.Width;
-                int height = img.Height;
+                int width = imgCopy.Width;
+                int height = imgCopy.Height;
                 int nChan = m.nChannels; // number of channels - 3
                 int padding = m.widthStep - m.nChannels * m.width; // alinhament bytes (padding)
                 int x, y;
@@ -143,7 +80,7 @@ namespace CG_OpenCV.Services
 
                             Color original = Color.FromArgb(red, green, blue);
                             ColorToHSV(original, out var hue, out var saturation, out var value);
-                            if (70 < hue && hue < 320) //DAR O TUNE AQUI
+                            if (150 < hue && hue < 320) //DAR O TUNE AQUI
                             {
                                 histogramY[y] = histogramY[y] + 1;
                                 histogramX[x] = histogramX[x] + 1;
@@ -162,7 +99,6 @@ namespace CG_OpenCV.Services
                     int thresholdPertenceTabuleiro = 3; //Alter threshold de percentagem
                     //Place the values by percentage on the histograms and see if they are the xo,yo or x1,y1 of the board.
                     // Initialize min and max values for X and Y
-                    int minX = -1, maxX = -1, minY = -1, maxY = -1;
 
                     // Process histogramX and find min and max X where histogramX[x] > 20
                     for (x = 0; x < width; x++)
@@ -191,14 +127,16 @@ namespace CG_OpenCV.Services
                             maxY = y;
                         }
                     }
-                    // Output the results
-                    Console.WriteLine($"Min X: {minX}, Max X: {maxX}");
-                    Console.WriteLine($"Min Y: {minY}, Max Y: {maxY}");
-
-                    return 
-                    
-
                 }
             }
+
+            //primeira coordenada ponta inferior esquerda 
+            //segunda coordenada ponta superior direita
+            return new Coord[2]
+            {
+                new Coord(minX,maxY),
+                new Coord(maxX,minY),
+            };
         }
+    }
 }
