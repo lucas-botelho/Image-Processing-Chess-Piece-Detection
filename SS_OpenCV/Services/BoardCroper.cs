2853e5dddacc53163873fbbc06b0a345840703c0
Tiago Mota <28tiago10c@gmail.com>
2024-06-02 17:20:33 +0100

    Adicionei findpiececoords

--- SS_OpenCV/Services/BoardCroper.cs
+++ SS_OpenCV/Services/BoardCroper.cs
@@ -138,7 +138,85 @@ namespace CG_OpenCV.Services
                 new Coord(maxX,minY),
             };
         }
+        public Coord[] FindPieceCoor()
+        {
+            int minX = -1, maxX = -1, minY = -1, maxY = -1;
 
+            unsafe
+            {
+                MIplImage m = imgCopy.MIplImage;
+                byte* dataPtr = (byte*)m.imageData.ToPointer(); // Pointer to the image
+                byte blue, green, red;
+                int width = imgCopy.Width;
+                int height = imgCopy.Height;
+                int nChan = m.nChannels; // number of channels - 3
+                int padding = m.widthStep - m.nChannels * m.width; // alinhament bytes (padding)
+                int x, y;
+                int step = m.widthStep;
+                float[] histogramX = new float[width]; //holds the percentage of 255 on that X column
+                float[] histogramY = new float[height]; //holds the percentage of 255 of that Y line
+                if (nChan == 3) // image in RGB RedGreenBlue
+                {
+
+                    for (y = 0; y < height; y++)
+                    {
+                        for (x = 0; x < width; x++)
+                        {
+                            //retrive 3 colour components
+                            blue = (dataPtr + x * nChan + y * step)[0];
+                            green = (dataPtr + x * nChan + y * step)[1];
+                            red = (dataPtr + x * nChan + y * step)[2];
+                            var ePreto = blue == 0 && green == 0 && red == 0;
+
+                            if (ePreto) //DAR O TUNE AQUI
+                            {
+                                histogramY[y] = histogramY[y] + 1;
+                                histogramX[x] = histogramX[x] + 1;
+                            } 
+                        }
+                    }
+                    int thresholdPertenceTabuleiro = 3; //Alter threshold de percentagem
+                    //Place the values by percentage on the histograms and see if they are the xo,yo or x1,y1 of the board.
+                    // Initialize min and max values for X and Y
+
+                    // Process histogramX and find min and max X where histogramX[x] > 20
+                    for (x = 0; x < width; x++)
+                    {
+                        histogramX[x] = (histogramX[x] / height) * 100;
+                        if (histogramX[x] > thresholdPertenceTabuleiro)
+                        {
+                            if (minX == -1)
+                            {
+                                minX = x;
+                            }
+                            maxX = x;
+                        }
+                    }
+
+                    // Process histogramY and find min and max Y where histogramY[y] > 20
+                    for (y = 0; y < height; y++)
+                    {
+                        histogramY[y] = (histogramY[y] / width) * 100;
+                        if (histogramY[y] > thresholdPertenceTabuleiro)
+                        {
+                            if (minY == -1)
+                            {
+                                minY = y;
+                            }
+                            maxY = y;
+                        }
+                    }
+                }
+            }
+
+            //primeira coordenada ponta inferior esquerda 
+            //segunda coordenada ponta superior direita
+            return new Coord[2]
+            {
+                new Coord(minX,maxX),
+                new Coord(minY,maxY),
+            };
+        }
 
         public IEnumerable<Image<Bgr, Byte>> CropIndividualPieces(Image<Bgr, Byte> croppedBoard)
         {
